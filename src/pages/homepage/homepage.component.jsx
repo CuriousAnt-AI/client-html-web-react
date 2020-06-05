e96c2455fcc398d127691a6d4de18e67e2f8a80a
Vky-pwl <vikaspawale.v@gmail.com>
2020-06-05 23:48:41 +0530

    Added: header | paceholder

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,12 +4,21 @@ import './homepage.styles.scss';
 const HomePage = () => {
     return (
         <div className='homepage'>
-            <div className="bg-video">
-                <video className="bg-video__content" autoPlay muted loop>
-                    <source src="assets/video.mp4" type="video/mp4" />
-                    <source src="assets/video.webm" type="video/webm" />
-                    Your browser is not supported!
-                    </video>
+            <div className="placeload">
+                <div className="placeload-header">
+                    <div className="img loads"></div>
+                    <div className="header-content">
+                        <div className="content-shape loads"></div>
+                        <div className="content-shape loads"></div>
+                    </div>
+                </div>
+                <div className="image-placeholder loads"></div>
+                <div className="placeholder-footer">
+                    <div className="footer-block">
+                        <div className="content-shape loads"></div>
+                        <div className="content-shape loads"></div>
+                    </div>
+                </div>
             </div>
         </div>
 
